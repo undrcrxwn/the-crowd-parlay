db6e9d01861571150fd945c9f1c5a26bef75be38
undrcrxwn <undrcrxwn@gmail.com>
2022-04-25 22:08:59 +0300

    Replace IdentityUser with ApplicationUser

--- src/CrowdParlay/Data/ApplicationDbContext.cs
+++ src/CrowdParlay/Data/ApplicationDbContext.cs
@@ -1,13 +1,13 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CrowdParlay.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext
+        : IdentityDbContext<ApplicationUser, IdentityRole, string>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+            : base(options) {}
     }
 }
